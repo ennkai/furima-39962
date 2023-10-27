2313fc06c520b578114b79b338b1b894b5ddb222
ennkai <ennkaiponn@outlook.jp>
2023-10-27 14:29:10 +0900

    その他カテゴリーの設定とバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,10 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   belongs_to :category
+  belongs_to :prefecture
+  belongs_to :item_status
+  belongs_to :shipping_cost
+  belongs_to :shipping_date
 
   validates :name, presence: true
   validates :description, presence: true
@@ -12,5 +16,13 @@ class Item < ApplicationRecord
   validates :shipping_region, presence: true
   validates :shipping_days, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+
+
+  validates :category_id, numericality: { other_than: 0 }
+  validates :prefecture_id, numericality: { other_than: 0 }
+  validates :item_status_id, numericality: { other_than: 0 }
+  validates :shipping_cost_id, numericality: { other than: 0 }
+  validates :shipping_date_id, numericality: { other_than: 0 }
+
 
 end
