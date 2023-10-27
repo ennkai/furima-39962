e4e2545084e1a89669e7525356fed30057d63c75
ennkai <ennkaiponn@outlook.jp>
2023-10-27 12:47:31 +0900

    カテゴリーの実装及ぶそのバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,16 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  has_one_attached :image
+
+  belongs_to :category
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :category, presence: true
+  validates :condition, presence: true
+  validates :shipping_cost, presence: true
+  validates :shipping_region, presence: true
+  validates :shipping_days, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+
 end
